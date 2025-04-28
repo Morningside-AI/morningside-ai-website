af1cda2efa81857a3b02bfe1409f3d74dbe43747
MedNewton <med.newton.2013@gmail.com>
2025-04-28 23:47:27 +0000

    .

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -286,9 +286,9 @@ const Footer = () => {
             <div
                 id="footer-section"
                 ref={footerRef}
-                className="w-full h-screen min-h-screen flex flex-col will-change-transform justify-between items-center text-white tracking-[-0.04em] leading-[90%] pt-6 sm:pt-4 lg:pt-6 overflow-hidden"
+                className="w-full h-screen min-h-screen flex flex-col will-change-transform justify-between items-center text-white tracking-[-0.04em] leading-[90%] pt-6 sm:pt-4 lg:pt-6 bg-cyan-700 sm:bg-amber-600 md:bg-blue-700 lg:bg-fuchsia-800 overflow-hidden"
             >
-                <div className="w-full flex flex-row justify-between" ref={textRef}>
+                <div className="w-full h-fit flex flex-row justify-between" ref={textRef}>
                     <p className="w-full lg:text-6xl text-5xl text-left leading-normal">
                         <span className="white-silver-animated-text">We&nbsp;</span>
                         <span className="white-silver-animated-text">look&nbsp;</span>
@@ -303,13 +303,13 @@ const Footer = () => {
                     <Logo className="w-48 h-10 mt-4 hidden lg:block" />
                 </div>
 
-                <div className="w-full flex lg:flex-row flex-col-reverse lg:items-baseline-last justify-between mb-16 sm:mb-0 md:mb-0 lg:mb-0 pb-4 sm:pb-0 md:pb-2 lg:pb-0 tracking-wider text-sm relative">
+                <div className="w-full h-fit flex lg:flex-row flex-col-reverse lg:items-baseline-last justify-between mb-16 sm:mb-0 md:mb-0 lg:mb-0 pb-4 sm:pb-0 md:pb-2 lg:pb-0 tracking-wider text-sm relative">
                     <div className="flex flex-col gap-1 lg:gap-2 order-3 md:order-1 footer-contact">
                         <p className="whitespace-pre-wrap font-bold text-[#D9D9D9] uppercase">Contact</p>
                         <Link href="https://mail.google.com/mail/?view=cm&to=info@morningside.ai&su=Morningside%20AI%20Contact%20Request&body=Hi%0A%0AI%20am%20reaching%20out%20from%20the%20Morningside%20AI%20website" target="_blank" className="w-full cursor-pointer decoration-0">
                             <p className="whitespace-pre-wrap text-white cursor-pointer hover:text-white/80 my-2">info@morningside.ai</p>
                         </Link>
-                        <div className="flex flex-row gap-2 relative z-10">
+                        <div className="flex flex-row gap-2 relative z-10 sm:mb-2 md:mb-28 lg:mb-10">
                             <button onClick={handleContactClick} className="flex cursor-pointer items-center gap-1 px-4 py-2 border border-white rounded-full text-white bg-transparent hover:bg-white hover:text-black whitespace-nowrap">
                                 Get In Touch
                                 <GoArrowUpRight size={18} strokeWidth={1} className="mt-1 transition-all duration-300" />
@@ -336,7 +336,7 @@ const Footer = () => {
                         </p>
                     </div>
 
-                    <div className="flex flex-col justify-end gap-2 lg:gap-4 order-3 md:order-1 text-left footer-follow">
+                    <div className="flex flex-col justify-end gap-2 lg:gap-4 order-3 md:order-1 text-left footer-follow mb-8">
                         <p className="whitespace-pre-wrap font-bold text-[#D9D9D9] uppercase">Follow</p>
                         <Link href="https://www.linkedin.com/company/morningside-ai/posts/?feedView=all" target="_blank" className="w-fit cursor-pointer hover:opacity-80">
                             <p className="whitespace-pre-wrap font-medium text-[#D9D9D9] uppercase hover:text-white/70">Linkedin</p>
