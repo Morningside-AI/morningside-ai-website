069a84d107e6460a955c86851ad31ec67355a621
Sly <james.oldham0604@gmail.com>
2025-04-29 16:35:00 +1200

    Redirects setup for /careers and /jobs to point to Tally Form and /team, /services and /blog to point to default site.

--- next.config.js
+++ next.config.js
@@ -3,9 +3,28 @@ import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
+  async redirects() {
+    return [
+      { source: "/team", destination: "/", permanent: true },
+      { source: "/services", destination: "/", permanent: true },
+      { source: "/blog", destination: "/", permanent: true },
+
+      {
+        source: "/careers",
+        destination: "https://tally.so/r/wbYr52",
+        permanent: true,
+      },
+      {
+        source: "/jobs",
+        destination: "https://tally.so/r/wbYr52",
+        permanent: true,
+      },
+    ];
+  },
   webpack(config) {
-    const fileLoaderRule = config.module.rules.find((/** @type {{ test: { test: (arg0: string) => any; }; }} */ rule) =>
-      rule?.test?.test?.(".svg")
+    const fileLoaderRule = config.module.rules.find(
+      (/** @type {{ test: { test: (arg0: string) => any; }; }} */ rule) =>
+        rule?.test?.test?.(".svg"),
     );
     if (fileLoaderRule) {
       fileLoaderRule.exclude = /\.svg$/i;
@@ -33,7 +52,7 @@ const config = {
         test: /\.svg$/i,
         resourceQuery: /url/,
         type: "asset/resource",
-      }
+      },
     );
 
     return config;
