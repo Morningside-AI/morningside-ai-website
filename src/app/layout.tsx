277a1b30c1e22fd5451e0ac789b1e702f8e2b0a4
Sly <james.oldham0604@gmail.com>
2025-04-29 16:23:17 +1200

    Vercel Analytics Tracking Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,18 @@ import AnimatedMeshBackground from "@/components/generic/AnimatedMeshBackground"
 import "@/styles/globals.css";
 import "@/styles/fonts.css";
 import { type Metadata } from "next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Morningside AI",
   description: "Morningside AI",
   icons: [
-    { rel: "icon", type: "image/png", sizes: "96x96", url: "/favicon-96x96.png" },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "96x96",
+      url: "/favicon-96x96.png",
+    },
     { rel: "icon", type: "image/svg+xml", url: "/favicon.svg" },
     { rel: "shortcut icon", url: "/favicon.ico" },
     { rel: "apple-touch-icon", sizes: "180x180", url: "/apple-touch-icon.png" },
@@ -21,7 +27,8 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className="bg-black relative" style={{ fontFamily: "DM-Sans" }}>
+      <body className="relative bg-black" style={{ fontFamily: "DM-Sans" }}>
+        <Analytics />
         <AnimatedMeshBackground />
         {children}
       </body>
