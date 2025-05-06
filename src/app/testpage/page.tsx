2cbcd6543c85f09289e4b394da47d0dd8c704907
MedNewton <med.newton.2013@gmail.com>
2025-05-06 12:34:24 +0000

    .

--- src/app/testpage/page.tsx
+++ src/app/testpage/page.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
 import { useRef } from "react";
-import PreloaderWrapper from "@/components/generic/preloaderWrapper";
+import NewPreloaderWrapper from "@/components/generic/NewPreloaderWrapper";
 import SnappySection1 from "@/components/SnappySection1";
 import SnappySection2 from "@/components/SnappySection2";
 import SnappySection4 from "@/components/SnappySection4";
@@ -9,26 +9,30 @@ import SnappySection5 from "@/components/SnappySection5";
 import SnappySection6 from "@/components/SnappySection6";
 
 export default function Home() {
-
   const scrollContainerRef = useRef<HTMLDivElement>(null);
 
   return (
-    <div ref={scrollContainerRef} className="snap-y snap-mandatory overflow-y-scroll h-screen w-full overflow-x-hidden ">
-      <div className="snap-always snap-center">
-        <SnappySection1 />
+    <NewPreloaderWrapper>
+      <div
+        ref={scrollContainerRef}
+        className="snap-y snap-mandatory overflow-y-scroll h-screen w-full overflow-x-hidden"
+      >
+        <div className="snap-always snap-center">
+          <SnappySection1 />
+        </div>
+        <div className="snap-always snap-center">
+          <SnappySection2 />
+        </div>
+        <div className="snap-always snap-center">
+          <SnappySection4 scrollContainerRef={scrollContainerRef} />
+        </div>
+        <div className="snap-always snap-center">
+          <SnappySection5 />
+        </div>
+        <div className="snap-always snap-center">
+          <SnappySection6 />
+        </div>
       </div>
-      <div className="snap-always snap-center" >
-        <SnappySection2 />
-      </div>
-      <div className="snap-always snap-center" >
-        <SnappySection4 scrollContainerRef={scrollContainerRef} />
-      </div>
-      <div className="snap-always snap-center" >
-        <SnappySection5 />
-      </div>
-      <div className="snap-always snap-center" >
-        <SnappySection6 />
-      </div>
-    </div>
+    </NewPreloaderWrapper>
   );
 }
---
+++ src/components/generic/NewPreloaderWrapper.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useState } from "react";
+import Preloader from "../preloader/preloader";
+
+const NewPreloaderWrapper = ({ children }: { children: React.ReactNode }) => {
+  const [isPreloaderDone, setIsPreloaderDone] = useState(false);
+
+  return (
+    <>
+      <Preloader onComplete={() => setIsPreloaderDone(true)} />
+      <div
+        className={`transition-opacity duration-700 ${
+          isPreloaderDone ? "opacity-100" : "opacity-0 pointer-events-none touch-none scroll-none overflow-hidden"
+        }`}
+      >
+        {children}
+      </div>
+    </>
+  );
+};
+
+export default NewPreloaderWrapper;
