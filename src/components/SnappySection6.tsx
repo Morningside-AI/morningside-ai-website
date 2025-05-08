a0ce2f92d67cf3306cd2eeaa6af9e86bef14a809
MedNewton <med.newton.2013@gmail.com>
2025-05-08 04:09:12 +0000

    .

--- src/components/SnappySection6.tsx
+++ src/components/SnappySection6.tsx
@@ -1,8 +1,9 @@
 import HoverVideo from "./video/hoverVideo";
+import ms from "@/assets/images/backgrounds/ms.webp"
 
 function SnappySection6() {
     return (
-        <div className={`relative h-[100dvh] min-h-[100dvh] w-full flex`}>
+        <div className={`relative h-[100dvh] min-h-[100dvh] w-full flex bg-cover bg-center`} style={{ backgroundImage: `url(${ms.src})` }}>
             <div className="w-full h-full">
                 <HoverVideo />
             </div>
