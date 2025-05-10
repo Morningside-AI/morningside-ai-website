539e8a8f5b52f9564d46fbb9f35797809ab24458
MedNewton <med.newton.2013@gmail.com>
2025-05-10 16:20:07 +0000

    tablet drawer width

--- src/components/partnership/partnership.tsx
+++ src/components/partnership/partnership.tsx
@@ -170,7 +170,7 @@ const Partnership = () => {
       {/* Drawer Component */}
       <div
         ref={drawerRef}
-        className="fixed right-0 md:right-0 top-5 md:top-[2.5vh] h-[85vh] md:h-[95vh] w-full md:w-[40vw] bg-[#EDECE4] text-black transform translate-x-full shadow-2xl z-50 rounded-xl overflow-hidden"
+        className="fixed right-0 md:right-0 top-5 md:top-[2.5vh] h-[85vh] md:h-[95vh] w-full md:w-[99vw] lg:w-[40vw] bg-[#EDECE4] text-black transform translate-x-full shadow-2xl z-50 rounded-xl overflow-hidden"
       >
         {/* Drawer Header */}
         <div className="flex flex-col px-4 md:px-6 py-6">
