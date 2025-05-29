da66eff48e0ba1881ebab3098f756472c8a1a9a6
MedNewton <med.newton.2013@gmail.com>
2025-05-29 23:41:22 +0000

    new footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -154,7 +154,7 @@ const Footer = () => {
                         <Logo className="w-48 h-0 lg:h-10 mt-0 lg:mt-4 hidden lg:block" />
                     </div>
 
-                    <div className="w-full h-fit flex lg:flex-row flex-col-reverse lg:items-baseline-last justify-between tracking-wider text-sm relative gap-8 lg:gap-0">
+                    <div className="w-full hidden lg:flex h-fit lg:flex-row flex-col-reverse lg:items-baseline-last justify-between tracking-wider text-sm relative gap-8 lg:gap-0">
                         <div className="flex flex-col w-full lg:w-1/3 gap-1 lg:gap-2 order-3 md:order-1 footer-contact">
                             <p className="whitespace-pre-wrap font-bold text-[#D9D9D9] uppercase">Contact</p>
                             <Link href="https://mail.google.com/mail/?view=cm&to=info@morningside.ai&su=Morningside%20AI%20Contact%20Request&body=Hi%0A%0AI%20am%20reaching%20out%20from%20the%20Morningside%20AI%20website" target="_blank" className="w-full cursor-pointer decoration-0">
@@ -195,6 +195,51 @@ const Footer = () => {
                             <Link href="https://www.youtube.com/@LiamOttley" target="_blank" className="w-fit cursor-pointer hover:opacity-80">
                                 <p className="whitespace-pre-wrap font-medium text-[#D9D9D9] uppercase hover:text-white/70">Youtube</p>
                             </Link>
+                        </div>
+                    </div>
+
+                    <div className="w-full flex lg:hidden h-fit lg:flex-row flex-col lg:items-baseline-last justify-between tracking-wider text-sm relative gap-8 lg:gap-0">
+                        <div className="flex flex-col w-full lg:w-1/3 gap-1 lg:gap-2 order-3 md:order-1 footer-contact">
+                            <p className="whitespace-pre-wrap font-bold text-[#D9D9D9] uppercase">Contact</p>
+                            <Link href="https://mail.google.com/mail/?view=cm&to=info@morningside.ai&su=Morningside%20AI%20Contact%20Request&body=Hi%0A%0AI%20am%20reaching%20out%20from%20the%20Morningside%20AI%20website" target="_blank" className="w-full cursor-pointer decoration-0">
+                                <p className="whitespace-pre-wrap text-white cursor-pointer hover:text-white/80 my-2">info@morningside.ai</p>
+                            </Link>
+                        </div>
+                        <div className="w-full lg:w-1/3 flex flex-col justify-end lg:items-end gap-2 lg:gap-4 order-3 md:order-1 text-left footer-follow">
+                            <p className="whitespace-pre-wrap text-[#D9D9D9] uppercase">Follow</p>
+                            <Link href="https://www.linkedin.com/company/morningside-ai/posts/?feedView=all" target="_blank" className="w-fit cursor-pointer hover:opacity-80">
+                                <p className="whitespace-pre-wrap font-medium text-[#D9D9D9] uppercase hover:text-white/70">Linkedin</p>
+                            </Link>
+                            <Link href="https://www.youtube.com/@LiamOttley" target="_blank" className="w-fit cursor-pointer hover:opacity-80">
+                                <p className="whitespace-pre-wrap font-medium text-[#D9D9D9] uppercase hover:text-white/70">Youtube</p>
+                            </Link>
+                        </div>
+                        <div className="flex flex-col w-full lg:w-1/3 gap-1 lg:gap-2 order-3 md:order-1 footer-contact">
+                            <div className="flex flex-row gap-2 relative z-10">
+                                <button onClick={toggleDrawer} className="flex cursor-pointer items-center gap-1 px-4 py-2 border border-white rounded-full text-white bg-transparent hover:bg-[#EDECE4] hover:text-black whitespace-nowrap">
+                                    Get In Touch
+                                    <GoArrowUpRight size={18} strokeWidth={1} className="mt-1 transition-all duration-300" />
+                                </button>
+                                <Link href="https://tally.so/r/wbYr52" target="_blank" className="w-full cursor-pointer">
+                                    <button className="flex items-center cursor-pointer gap-1 px-4 py-2 border border-white rounded-full text-white bg-transparent hover:bg-[#EDECE4] hover:text-black whitespace-nowrap">
+                                        Explore Careers
+                                        <GoArrowUpRight size={18} strokeWidth={1} className="mt-1 transition-all duration-300" />
+                                    </button>
+                                </Link>
+                            </div>
+                        </div>
+
+                        <div className="w-full lg:w-1/3 flex flex-col lg:flex-row items-start lg:items-end lg:justify-center gap-2 lg:gap-4 order-3 lg:order-1">
+                            <p className="whitespace-pre-wrap font-medium text-[#D9D9D9] uppercase hover:opacity-70">
+                                <Link href="/terms-and-conditions" target="_blank" className="w-full cursor-pointer">
+                                    Terms & Conditions
+                                </Link>
+                            </p>
+                            <p className="whitespace-pre-wrap font-medium text-[#D9D9D9] uppercase hover:opacity-70">
+                                <Link href="/privacy-policy" target="_blank" className="w-full cursor-pointer ">
+                                    Privacy Policy
+                                </Link>
+                            </p>
                         </div>
                     </div>
                 </div>
