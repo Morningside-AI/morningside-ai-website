8211772ff3f1002bb780862747c87cef9bc4f667
MedNewton <med.newton.2013@gmail.com>
2025-05-30 00:04:11 +0000

    .

--- src/components/partnership/partnership.tsx
+++ src/components/partnership/partnership.tsx
@@ -237,9 +237,9 @@ const Partnership = () => {
       {/* Drawer Component */}
       <div
         ref={drawerRef}
-        className="fixed right-0 md:right-0 top-0 md:top-[0vh] h-[100vh] md:h-[100vh] lg:h-[100vh] w-full lg:w-[40vw] px-2 flex flex-row items-center justify-center text-black transform translate-x-full z-50"
+        className="fixed right-0 md:right-0 top-0 md:top-[0vh] h-[95vh] md:h-[100vh] lg:h-[100vh] w-full lg:w-[40vw] px-2 flex flex-row items-center justify-center text-black transform translate-x-full z-50"
       >
-        <div className="shadow-2xl rounded-xl w-[96%] md:w-[98%] lg:w-full h-[98%] bg-[#EDECE4] overflow-hidden">
+        <div className="shadow-2xl rounded-xl w-[96%] md:w-[98%] lg:w-full h-[90%] bg-[#EDECE4] overflow-hidden">
           {/* Drawer Header */}
           <div className="flex flex-col px-4 md:px-6 py-2">
             <div className="flex flex-row justify-end">
@@ -261,7 +261,7 @@ const Partnership = () => {
               {!success ? (
                 <>
                   <ContactForm setSuccess={setSuccess} />
-                  <div className="h-[80px] w-full"></div>
+                  <div className="h-[100px] w-full"></div>
                 </>
               ) : (
                 <div className="flex flex-col">
